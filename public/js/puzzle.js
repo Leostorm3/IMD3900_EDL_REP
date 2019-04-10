d6585c53878e1929d1032583bb30c3effb3f88df
Leostorm <etienne.doucet.leger@gmail.com>
2019-04-10 00:37:41 -0400

    added finale in puzzle

--- public/js/puzzle.js
+++ public/js/puzzle.js
@@ -3,7 +3,16 @@ var keychecker = false;
 var heartchecker = false;
 
 var Balance_weight;
-var Banlance_answer = 150;
+var Banlance_answer = 55;
+var balance_cheker=false;
+var color_order=false;
+var keycheker=false;
+var texter= false;
+var add_w1 = false;
+var add_w2 = false; 
+var add_w3 = false; 
+
+
 
 AFRAME.registerComponent('pick-me',
     {
@@ -70,12 +79,39 @@ AFRAME.registerComponent('pick-me',
         play: function() {
           this.el.addEventListener('drag-drop', function(evt) {
             var Weight = evt.detail.dropped.id;
-            
-            if(Key == "heart"){
-            evt.detail.dropped.setAttribute('material', 'color',
-              '#'+(Math.random()*0xFFFFFF<<0).toString(16))
+            var NumberWeight = Balance_weight;
+            if(Weight == "Weight1"){
+              if(add_w1 ==false){
+                Balance_weight = Balance_weight + 10;
+                add_w1 = true;
+              }
+              
+              
+
              // notify super-hands that the gesture was accepted
             evt.preventDefault()
+            }
+
+            if(Weight == "Weight2"){
+              if(add_w2 ==false){
+                Balance_weight = Balance_weight + 45;
+                add_w2 = true;
+              }
+
+             // notify super-hands that the gesture was accepted
+            evt.preventDefault()
+            }
+            if(Weight == "Weight3"){
+              if(add_w3 ==false){
+                Balance_weight = Balance_weight + 30;
+                add_w3 = true;
+              }
+             // notify super-hands that the gesture was accepted
+            evt.preventDefault()
+            }
+            if (Balance_weight ==Banlance_answer){
+              balance_cheker = true;
+              document.getElementById("text").setAttribute('value', "Weight puzzle done");
             }
           })
         }
@@ -87,6 +123,26 @@ AFRAME.registerComponent('pick-me',
 
             el.addEventListener('click', function () {
               Balance_weight=0; 
+              document.getElementById("Weight1").setAttribute('position',-8 + " " + 1.8 + " " + 6);
+              document.getElementById("Weight2").setAttribute('position',-8.5 + " " + 1.8 + " " + 6);
+              document.getElementById("Weight3").setAttribute('position',-9 + " " + 1.8 + " " + 6);
+        
+      });
+        }  
+      });
+
+      AFRAME.registerComponent('Check-answer', {
+        init: function () {
+          var el = this.el;
+          var cam =  document.getElementById("head");
+
+            el.addEventListener('click', function () {
+             if (balance_cheker ==true && color_order==true && texter==true && keycheker == true){
+              cam.setAttribute('look-controls', "enabled:false");
+              cam.setAttribute('animation', "property: position; to: 5 1.6 0; dur: 1500; easing: linear");
+              document.getElementById("textH").setAttribute('value', "And now you are free....");
+
+             }
         
       });
         }  
